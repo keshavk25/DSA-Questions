048300c80ef1977ad2774c2c1fcc8e27f1bb3578
keshav kumar <keshavks9810@gmail.com>
2024-11-19 10:43:36 +0530

    Permutation algorithm

---
+++ code/permutation.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<vector>
+#include <algorithm>
+
+using namespace std;
+
+void per(int ind,int arr[3], int n){
+    if(ind== n){
+           
+        for(int i=0;i<3;i++){
+            cout<<arr[i];
+        } 
+        cout<<endl;
+        return;
+    }
+
+    for(int i = ind; i<n ; i++){
+        swap(arr[ind],arr[i]);
+        per(ind+1,arr,3);
+        swap(arr[ind],arr[i]);
+    }
+}
+
+int main(){
+   int arr[3]={1,2,3};
+    int ans[3][3];
+    per(0,arr,3);
+
+
+return 0;
+}
